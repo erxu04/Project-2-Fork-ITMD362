ebfa46a12c558e8324578069b2701ac8213eadac
joenwoke <jnwoke@hawk.iit.edu>
2024-04-05 11:47:40 -0500

    Testing slider on local repo

--- js/script.js
+++ js/script.js
@@ -39,3 +39,20 @@ function init(){
 }
 
 window.addEventListener('load', init)
+
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 5 seconds
+}
+
+showSlides();
