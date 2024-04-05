1a3051bd300ad05b0c7623ba6027bfc37b743255
joenwoke <jnwoke@hawk.iit.edu>
2024-04-05 12:14:56 -0500

    Increased slider time for images

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 5 seconds
+  setTimeout(showSlides, 15000); // Change image every 5 seconds
 }
 
 showSlides();
